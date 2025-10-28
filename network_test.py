5a3ac778499910888292931a6a5d32a90d285b19
Jeremy Mill <jeremymill@gmail.com>
2025-10-28 09:51:29 -0400

    get the ips and mac addresses

--- network_test.py
+++ network_test.py
@@ -1,3 +1,36 @@
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # first we need to know what hosts on are on the LAN
 # let's check ARP first before we port sc
+def get_arp_via_subprocess() -> tuple[set[str], set[str]]:
+    ips = set()
+    macs = set()
+    # run arp -a and capture output
+    result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
+    output = result.stdout
+    for line in output.splitlines():
+        # pull IPs out via regex
+        # sample output line: "? (192.168.68.56) at e6:c0:b:4b:d:26 on en0 ifscope [ethernet]"
+        first_paren = line.find('(')
+        closing_paren = line.find(')')
+        if first_paren == -1 or closing_paren == -1:
+            logging.warning("Could not find parentheses in line: %s", line)
+            continue
+        ip = line[first_paren + 1:closing_paren]
+        start_mac = line.find(' at ') + 4
+        end_mac = line.find(' on ')
+        if start_mac == -1 or end_mac == -1:
+            logging.warning("Could not find MAC address in line: %s", line)
+            continue
+        mac = line[start_mac:end_mac]
+        ips.add(ip)
+        macs.add(mac)
+        logging.info(f"IP Address: %s, MAC Address: %s", ip, mac)
+
+    return ips, macs
+
+if __name__ == "__main__":
+    get_arp_via_subprocess()
